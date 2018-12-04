09e853aabc0d2f37541651562556b31489d1542f
Jan Niestadt <jan.niestadt@inl.nl>
2018-12-04 13:53:57 +0100

    Backend toegevoegd.

---
+++ backend/kwetter.php
@@ -0,0 +1,30 @@
+<?php
+
+// Where to store messages
+define("MESSAGES_FILE", "/tmp/messages.json");
+
+$messages = [];
+if (file_exists(MESSAGES_FILE))
+    $messages = json_decode(file_get_contents(MESSAGES_FILE));
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Post a new message
+    $message = [
+        "message" => $_POST['message'],
+        "user" => $_POST['user'],
+        "time" => $_POST['time']
+    ];
+    array_push($messages, $message);
+
+    // Write the new messages file
+    file_put_contents(MESSAGES_FILE, json_encode($messages, JSON_PRETTY_PRINT));
+
+} else {
+
+    // Return the messages so far
+    header("Content-Type: application/json");
+    print json_encode($messages);
+
+}
+
+?>
